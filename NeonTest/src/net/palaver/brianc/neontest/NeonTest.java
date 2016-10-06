38a51f1fa8b11a892c1b87e08c306991846ea4ed
brianc <brianc@palaver.net>
2016-10-06 01:54:14 -0400

    Final change for Thursday morning

--- NeonTest/src/net/palaver/brianc/neontest/NeonTest.java
+++ NeonTest/src/net/palaver/brianc/neontest/NeonTest.java
@@ -3,7 +3,7 @@ package net.palaver.brianc.neontest;
 public class NeonTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hellow Orld!!  I am running in Neon");
+		System.out.println("Hellow Orld!!  I am running in Neon ane connected to git");
 
 	}
 
