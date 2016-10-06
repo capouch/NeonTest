a9898ffdd6a9a9ded5479a3c684b45abfa110173
brianc <brianc@localhost>
2016-10-06 01:52:05 -0400

    Make something happen

--- NeonTest/src/net/palaver/brianc/neontest/NeonTest.java
+++ NeonTest/src/net/palaver/brianc/neontest/NeonTest.java
@@ -3,7 +3,7 @@ package net.palaver.brianc.neontest;
 public class NeonTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hellow Orld!!  I am running in Neon");
 
 	}
 
